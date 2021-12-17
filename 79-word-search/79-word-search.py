b2526ac07bace466f73c3ea66d2a430beed99382
Ian Moskunas <ianmosk19@gmail.com>
2021-12-17 17:46:36 -0500

    Time: 9683 ms (5.00%), Space: 14.3 MB (74.14%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,35 +1,31 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        def dfs(board, row, col, currWord):
+        
+        # dfs for every letter in grid
+        # return false if OOB or not desired letter
+        # return true if no letters left to check for 
+        # dfs will return OR for each direction
+        # side pt: have to null out where we've searched before each dfs
+        
+        def dfs(i, j, word):
             
-            # no letters left to check for, mean word was found
-            if len(currWord) == 0:
+            if len(word) == 0:
                 return True
             
-            if (row < 0 or row >= len(board)) or (col < 0 or col >= len(board[0])) or (currWord[0] != board[row][col]):
+            if (i < 0 or i >= len(board)) or (j < 0 or j >= len(board[0])) or (word[0] != board[i][j]):
                 return False
             
-            # adjust what word currently is
-            # also avoid using this value again for later DFS
-            tmp = board[row][col]
-            board[row][col] = '0'
-            
-            res = dfs(board, row-1, col, currWord[1:]) or \
-            dfs(board, row+1, col, currWord[1:]) or \
-            dfs(board, row, col-1, currWord[1:]) or \
-            dfs(board, row, col+1, currWord[1:])
-            
-            # reinstate that value null'd from above for other searches
-            board[row][col] = tmp
-            
+            tmp = board[i][j]
+            board[i][j] = 0
+            res = dfs(i-1, j, word[1:]) or dfs(i+1, j, word[1:]) or dfs(i, j-1, word[1:]) or dfs(i, j+1, word[1:])
+            board[i][j] = tmp
             return res
         
-        # dfs check every letter to see if word exists
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if dfs(board, i, j, word):
+                if dfs(i, j, word):
                     return True
-        return False
                     
         

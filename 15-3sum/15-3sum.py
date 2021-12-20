dab0f2dce7da63d046a0b7183ac086345f1b0b87
Ian Moskunas <ianmosk19@gmail.com>
2021-12-20 15:39:21 -0500

    Time: 2264 ms (32.17%), Space: 16.8 MB (99.63%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,25 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # iterate over the sorted array
-        # save current val as first portion of target
-        # closing window from currIdx+1 -> len(nums) - 1
-        # since sorted, can close that window depending on what values needed
-        answers = set()
-        nums = sorted(nums)
-        for i in range(len(nums)):
-            left, right = i + 1, len(nums) - 1
-            target = 0 - nums[i]
-            while left < right:
-                # have found viable triplet and can check further
-                if nums[left] + nums[right] == target:
-                    answers.add((nums[i], nums[left], nums[right]))
-                    left += 1
-                    right -= 1
-                # need a larger number for possible triplet
-                elif nums[left] + nums[right] < target:
-                    left += 1
-                # need a smaller number for possible triplet
-                else:
-                    right -= 1
-        return list(answers)
+
+        # iterate idx of nums
+        # curr target will idx, then two ptrs finding any eligible candidates
         
+        ans = set()
+        nums.sort()
+        
+        for i in range(len(nums)):
+            
+            currTarget = 0 - nums[i]
+            l, r = i + 1, len(nums) - 1
+            
+            while l < r:
+                
+                # have viable triplet to add
+                if nums[l] + nums[r] == currTarget:
+                    ans.add((nums[l], nums[r], nums[i]))
+                    l += 1
+                    r -= 1
+                
+                # need a larger number to try and reach target
+                elif nums[l] + nums[r] < currTarget:
+                    l += 1
+                    
+                # need a smaller number to try and reach target
+                else:
+                    r -= 1
+                    
+        return list(ans)    
+        
+                
